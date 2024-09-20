de7b35fc265c44b46330309a149584b155c12674
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-09-20 13:18:59 +0700

    Add reproduction for deprecated meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 /** Add your relevant code here for the issue to reproduce */
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  appleWebApp: {
+    capable: true,
+  },
+};
+
 export default function Home() {
   return null;
 }
